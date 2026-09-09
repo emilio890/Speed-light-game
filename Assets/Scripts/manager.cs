f80885a57864be50cdee3fc6ae500f93e8996ca1
gomezmaganaemilio-eng <emilio.magana@saei.mx>
2026-09-08 23:50:41 -0600

    2° commit
    Interacción entre el game over y la pantalla de inicio. Quit y inicio del botón de pausa

--- Assets/Scripts/manager.cs
+++ Assets/Scripts/manager.cs
@@ -1,29 +1,61 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class manager : MonoBehaviour
 {
-   public static manager instance;
+    [SerializeField]
+    private InputAction esc;
+    public static manager instance;
     private int score;
     [SerializeField]
     private TMP_Text Scoretext;
     [SerializeField]
     private GameObject gameover;
+    [SerializeField]
+    private GameObject pnaleinicio;
+    [SerializeField]
+    private GameObject pauseoanel;
+    public static bool iniciojuego = false;
+    private bool pausaon = false;
 
-
+    private void OnEnable()
+    {
+        esc.Enable();
+    }
+    private void OnDisable()
+    {
+        esc.Disable();
+    }
     private void Awake()
     {
         instance = this;
     }
     void Start()
     {
-        Time.timeScale = 1f;
+        
+        if (iniciojuego == true)
+        {
+            pnaleinicio.SetActive(false);
+            playbutton();
+            iniciojuego = false;
+        }
+        else
+        {
+            pnaleinicio.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (esc.triggered)
+        {
+            pausa();
+            pausaon = true;
+        }
     }
     public void loose()
     {
@@ -33,5 +65,43 @@ public class manager : MonoBehaviour
     {
         score += scores;
         Scoretext.text = "Score:" + score.ToString();
+    }
+
+    
+    public void playbutton()
+    {
+        pnaleinicio.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void retrybottom()
+    {
+        iniciojuego = true;
+        SceneManager.LoadScene(0);
+    }
+    public void reloadmenubutton()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void quitbutton()
+    {
+        Application.Quit();
+    }
+    public void pausa()
+    {
+        if (pausaon == false)
+        {
+
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            pauseoanel.SetActive(true);
+        }
+    }
+    public void despausa()
+    {
+        pausaon = false;
+        Time.timeScale = 1f;
+        pauseoanel.SetActive(false);
     }
 }
